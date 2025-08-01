810ae9d2027dced16cbe750b82862fbc6c1b234a
Atul Prakash <berealatul@gmail.com>
2025-08-01 13:08:16 +0530

    updated channel

--- srsRANBM/updateChannelEstimates.m
+++ srsRANBM/updateChannelEstimates.m
@@ -1,42 +1,46 @@
 function csi = updateChannelEstimates(csi,carrier,SRS,H)
-% Update record of channel estimates obtained via SRS
+% Update record of channel estimates obtained via SRS, with condition number check.
 
-    for ue = SRS
+    for ue_idx = 1:numel(SRS)
+        ue = SRS(ue_idx);
+        
+        H_ue_new = H{ue_idx};
+        
+        % Get the indices of the valid channel estimates
+        idx_new = find(all(~isnan(H_ue_new),[3 4]));
 
-        H_ue = H{ue==SRS};
-        idx = find(all(~isnan(H_ue),[3 4]));
-        csi(ue).H(idx,:,:,:) = H_ue(idx,:,:,:);
-        csi(ue).NSlot(idx) = carrier.NSlot;
+        if isempty(idx_new)
+            continue; % No new channel estimate, so skip
+        end
 
+        % Extract the relevant part of the new channel estimate
+        H_new_active = H_ue_new(idx_new,:,:,:);
 
+        % Get the old channel estimate for the same indices
+        H_old_active = csi(ue).H(idx_new,:,:,:);
 
+        % Handle the initial case where the old CSI is all NaNs
+        if all(isnan(H_old_active(:)))
+            cond_old = Inf;
+        else
+            % Calculate condition number for the old channel estimate
+            % We take the condition number of each sub-matrix and average them.
+            cond_old_vals = arrayfun(@(i) cond(squeeze(H_old_active(i,:,:,:))), 1:size(H_old_active,1));
+            cond_old = mean(cond_old_vals);
+        end
 
+        % Calculate condition number for the new channel estimate
+        cond_new_vals = arrayfun(@(i) cond(squeeze(H_new_active(i,:,:,:))), 1:size(H_new_active,1));
+        cond_new = mean(cond_new_vals);
+
+        % Update CSI if new channel is better (lower condition number)
+        if cond_new < cond_old
+            csi(ue).H(idx_new,:,:,:) = H_new_active;
+            csi(ue).NSlot(idx_new) = carrier.NSlot;
+            disp(['UE ' num2str(ue) ': CSI updated (new cond=' num2str(cond_new) ' < old cond=' num2str(cond_old) ')']);
+        else
+            disp(['UE ' num2str(ue) ': CSI not updated (new cond=' num2str(cond_new) ' >= old cond=' num2str(cond_old) ')']);
+        end
     end
 
 end
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% function csi = updateChannelEstimates(csi,carrier,SRS,H,activeAntennaIdx)
-% % Update record of channel estimates obtained via SRS
-% 
-%     for k = 1:numel(SRS)
-%         ue = SRS(k);
-%         H_ue = H{k};
-%         antIdx = activeAntennaIdx(k);
-% 
-%         % Defensive: assign only the correct antenna slice
-%         if ndims(H_ue) == 4
-%             csi(ue).H(:,:,:,antIdx) = H_ue(:,:,:,antIdx);
-%         elseif ndims(H_ue) == 3
-%             csi(ue).H(:,:,:,antIdx) = H_ue;
-%         else
-%             error('Unexpected dimensions for H_ue');
-%         end
-% 
-%         % Update slot info as before
-%         idx = find(all(~isnan(H_ue(:,:,:,min(end,antIdx))),[3])); % 3rd dim is subcarrier
-%         csi(ue).NSlot(idx) = carrier.NSlot;
-%     end
-% end
